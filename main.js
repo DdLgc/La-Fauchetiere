7e037ee8ef120a9b6bf09c97800f6c0f5203dd56
David Lgc <legouellecd@gmail.com>
2024-03-28 18:58:03 +0100

    Add truncate function on description card race

--- main.js
+++ main.js
@@ -1,2 +1,23 @@
+
 const currentYear = new Date().getFullYear();
   document.getElementById('copyright').textContent = `Copyright © ${currentYear} Ddlgc`;
+
+
+
+
+function truncateText(selector, maxLength) {
+  var elements = document.querySelectorAll(selector);
+  for (var i = 0; i < elements.length; i++) {
+    var text = elements[i].innerText;
+    if (text.split(' ').length > maxLength) {
+      text = text.split(' ').slice(0, maxLength).join(' ') + '...';
+      elements[i].innerText = text;
+    }
+  }
+}
+
+// Appel de la fonction pour tronquer le texte sur les petits écrans
+if (window.innerWidth > 768) {
+  truncateText('.js-truncate', 30);
+
+}
