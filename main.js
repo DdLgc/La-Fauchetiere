aad1d26934d8dc8f18b5e247dc9751ccad7dfe7e
David Lgc <legouellecd@gmail.com>
2024-04-08 22:51:20 +0200

    Add adjustTruncate function

--- main.js
+++ main.js
@@ -5,19 +5,28 @@ const currentYear = new Date().getFullYear();
 
 
 
-function truncateText(selector, maxLength) {
-  var elements = document.querySelectorAll(selector);
-  for (var i = 0; i < elements.length; i++) {
-    var text = elements[i].innerText;
-    if (text.split(' ').length > maxLength) {
-      text = text.split(' ').slice(0, maxLength).join(' ') + '...';
-      elements[i].innerText = text;
-    }
+  function truncateText(selector, maxLength) {
+    var elements = document.querySelectorAll(selector);
+    for (var i = 0; i < elements.length; i++) {
+      var element = elements[i];
+      var text = element.innerText;
+      var words = text.split(' ');
+  
+      if (words.length > maxLength) {
+        text = words.slice(0, maxLength).join(' ') + '...';
+        element.innerText = text;
+      }
+  }
+}
+function adjustTruncate() {
+  var screenWidth = window.innerWidth;
+  if (screenWidth >= 767 && screenWidth <= 1111) {
+    truncateText('.js-truncate', 17); 
+  } else {
+    truncateText('.js-truncate', 39); 
   }
 }
 
-// Appel de la fonction pour tronquer le texte sur les petits écrans
-if (window.innerWidth > 768) {
-  truncateText('.js-truncate', 30);
 
-}
+document.addEventListener("DOMContentLoaded", adjustTruncate);
+window.addEventListener('resize', adjustTruncate);
