eab601224186a02ad19433b2c1e0a4a8169d36f8
David Le Gouellec <legouellecd@gmail.com>
2026-07-08 22:47:09 +0200

    feat: modify JSON template

--- JS/racePage.js
+++ JS/racePage.js
@@ -1,7 +1,7 @@
-// class RacePAge {
-//     constructor(jsonPath, breedKey){
-//         this.jsonPath = jsonPath;
-//         this.breedKey = breedKey;
-//         this.content
-//     }
-// }
+class RacePAge {
+    constructor(jsonPath, breedKey){
+        this.jsonPath = jsonPath;
+        this.breedKey = breedKey;
+        this.content
+    }
+}
